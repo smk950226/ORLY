6c118f99606763fd2ac3253c427883afc789310e
smk950226 <smk950226@naver.com>
2017-11-26 03:20:15 +0900

    bootstrap적용

--- orly/settings/common.py
+++ orly/settings/common.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'cover',
 ]
 
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    ROOT('orly', 'static'),
+]
